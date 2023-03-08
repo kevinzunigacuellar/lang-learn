9a552583c46b7398ee67f451bc989e110583dfee
Dom Gasperini <38026244+dom-gasperini@users.noreply.github.com>
2023-03-08 17:35:17 -0500

    post creation now updates the database (#15)

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -5,3 +5,10 @@ export const loginSchema = zfd.formData({
   username: zfd.text(z.string().min(3).max(20)),
   password: zfd.text(z.string().min(8).max(10)),
 });
+
+export const postSchema = zfd.formData({
+  question: zfd.text(z.string().min(5).max(255)),
+  difficulty: zfd.text(z.enum(["Easy", "Medium", "Hard"])),
+  topic: zfd.text(z.enum(["Politics", "Daily Life", "Philosophy"])),
+  language: zfd.text(z.enum(["Spanish", "French", "English"])),
+});
---
+++ src/pages/api/posts.ts
@@ -0,0 +1,44 @@
+import type { APIRoute } from "astro";
+import { postSchema } from "../../lib/schemas";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export const post: APIRoute = async ({ request }) => {
+  const postData = await request.formData();
+  console.log(postData);
+  const result = postSchema.safeParse(postData);
+
+  if (!result.success) {
+    return new Response(
+      JSON.stringify({
+        errors: result.error.flatten(),
+      }),
+      { status: 400 }
+    );
+  }
+
+  // gather post data
+  const { question, difficulty, topic, language } = result.data;
+  const user = prisma.user.findUnique({
+    where: {
+      id: "0",
+    },
+  });
+
+  // send data to db
+  await prisma.posts.create({
+    data: {
+      question: question,
+      post_language: language,
+      has_response: false,
+      user_id: "cleyl3tpx0000o7uo25vuq383",
+    },
+  });
+
+  return new Response(
+    JSON.stringify({
+      message: "Successfully created post & saved to DB",
+    }),
+    { status: 200 }
+  );
+};
