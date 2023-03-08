38ffdeb96de6c8eaf219713b257fb94b5b984acc
Kevin Zuniga Cuellar <46791833+kevinzunigacuellar@users.noreply.github.com>
2023-03-08 16:35:12 -0500

    Add login (#12)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), solidJs()],
+  output: "server",
+  adapter: vercel()
+});
