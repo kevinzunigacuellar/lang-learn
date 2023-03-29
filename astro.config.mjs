dde9cc70aea6fcd02839b72f5ed860f1e1f0fb81
mlalongi <matthew.alongi@uvm.edu>
2023-03-29 16:32:23 -0400

    react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel/serverless";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()],
+  integrations: [tailwind(), solidJs(), react()],
   output: "server",
   adapter: vercel()
 });
