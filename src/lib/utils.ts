34d3b21d341a7e61562ef10f9388d6e930d3cb1d
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-04-05 17:18:18 -0400

    create-the-the-function

---
+++ src/lib/utils.ts
@@ -0,0 +1,15 @@
+import { auth } from "./firebase/server";
+
+type UserId = string;
+
+export async function getUserIdFromCookie(cookie: string) : Promise<UserId | null> {
+  try {
+    /* Verify the cookie. */
+    const decodedToken = await auth.verifySessionCookie(cookie, true);
+    /* Token is valid. Return the user's ID. */
+    return decodedToken.uid;
+  } catch(err) {
+    /* Session cookie is unavailable or invalid */
+    return null;
+  }
+}
