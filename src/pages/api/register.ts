94255f52a5e0da69d0b715b6cdb9413007872e85
Dom Gasperini <38026244+dom-gasperini@users.noreply.github.com>
2023-04-11 13:54:01 -0400

    updated signup (#29)

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -1,7 +1,12 @@
+// imports
 import { auth } from "../../lib/firebase/server";
 import type { APIRoute } from "astro";
 import { registerSchema } from "../../lib/schemas";
+import prisma from "../../lib/prisma";
+import { getUserIdFromCookie } from "../../lib/utils";
 
+
+// register function
 export const post: APIRoute = async ({ request, redirect }) => {
   const formData = await request.formData();
   const result = registerSchema.safeParse(formData);
@@ -17,14 +22,28 @@ export const post: APIRoute = async ({ request, redirect }) => {
   }
 
   /* Create the user */
-  const { email, password, name } = result.data;
+  const { email, password, name, username, targetLanguage } = result.data;
 
   try {
+    // add usr to firebase
     await auth.createUser({
       email,
       password,
       displayName: name,
     });
+    
+    // add user to database
+    await prisma.user.create({
+      data: {
+        name: name,
+        username: username,
+        password: "no passwords here",
+        email: email,
+        target_language: targetLanguage,
+        id: String(getUserIdFromCookie(document.cookie)),
+      },
+    });
+
   } catch (error: any) {
     return new Response(
       JSON.stringify({
