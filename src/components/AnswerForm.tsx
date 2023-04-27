b5037e3ec4c604614065c410eefa48f2e5d8f528
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-04-27 13:22:01 -0400

    add types helper

--- src/components/AnswerForm.tsx
+++ src/components/AnswerForm.tsx
@@ -1,6 +1,7 @@
-import { Suspense, createSignal, createResource, Show } from "solid-js";
+/** @jsxImportSource solid-js */
+
+import { createSignal, createResource } from "solid-js";
 import { answerSchema } from "../lib/schemas";
-import "./style.css";
 
 // Server request that submits a user's answer to a question
 async function answerFormData(formData: FormData) {
@@ -15,7 +16,7 @@ async function answerFormData(formData: FormData) {
 }
 
 // Form for submitting an answer to a question
-export default function AnswerForm({ post_id }) {
+export default function AnswerForm({ post_id }: { post_id: string }) {
   // post_id is passed in from the documentId in the question page
   // Create a signal for the form data and errors
   const [formData, setFormData] = createSignal<FormData>();
@@ -39,25 +40,22 @@ export default function AnswerForm({ post_id }) {
 
   // HTML for the form
   return (
-    <form onSubmit={submit}>
-      <div class="flex-container">
-        <div class="input p-4">
-          <label class="selector-label" for="question">
-            Respond to the Question:
-          </label>
-          <textarea
-            id="answer_content"
-            name="answer_content"
-            rows={6}
-            class="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
-          />
-        </div>
-      </div>
-      <div class="text-center" style="display: flex; justify-content: center;">
-        <button type="submit" class="btn-97">
-          Post
-        </button>
-      </div>
+    <form onSubmit={submit} class="mt-4 flex flex-col gap-2">
+      <label class="font-medium text-zinc-800" for="question">
+        Respond to the Question:
+      </label>
+      <textarea
+        id="answer_content"
+        name="answer_content"
+        rows={6}
+        class="block w-full mt-1 rounded-md text-zinc-700 border border-zinc-300 focus:border-pink-400 focus:outline-none focus:ring-2 focus:ring-pink-300 focus:bg-white"
+      />
+      <button type="submit"
+      disabled={response.loading}
+          class="bg-pink-500 py-1.5 rounded-md mt-1 text-white font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-400 disabled:opacity-50 disabled:cursor-not-allowed hover:bg-pink-600"
+          >
+        Post
+      </button>
     </form>
   );
 }
